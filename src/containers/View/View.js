7d3630b5fc80c66433fd369be27b0692ac78938b
William Kasel <wkasel@gmail.com>
2019-11-19 15:55:19 -0800

    Full MVP

--- src/containers/View/View.js
+++ src/containers/View/View.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import html2canvas from 'html2canvas';
 import LogoComponent from './LogoComponent';
 
 class View extends React.Component {
@@ -10,17 +11,50 @@ class View extends React.Component {
       textL1: 'Meth.',
       textL2: "We're on it."
     };
-    this.handleChangeL1 = this.handleChangeL1.bind(this);
-    this.handleChangeL2 = this.handleChangeL2.bind(this);
+
+    this.captureRef = React.createRef();
+    this.displayRef = React.createRef();
   }
 
-  handleChangeL1(event) {
+  handleChangeL1 = event => {
     this.setState({ textL1: event.target.value });
-  }
+  };
 
-  handleChangeL2(event) {
+  handleChangeL2 = event => {
     this.setState({ textL2: event.target.value });
-  }
+  };
+
+  saveAs = (uri, filename) => {
+    const link = document.createElement('a');
+    if (typeof link.download === 'string') {
+      link.href = uri;
+      link.download = filename;
+
+      // Firefox requires the link to be in the body
+      document.body.appendChild(link);
+
+      // simulate click
+      link.click();
+
+      // remove the link when done
+      document.body.removeChild(link);
+    } else {
+      window.open(uri);
+    }
+  };
+
+  getScreenshotHandler = () => {
+    html2canvas(this.captureRef.current).then(
+      canvas =>
+        this.saveAs(
+          canvas
+            .toDataURL('image/jpeg')
+            .replace('image/jpeg', 'image/octet-stream'),
+          'were-on-it-generator.jpg'
+        )
+      // console.log(canvas)
+    );
+  };
 
   render() {
     const { textL1, textL2 } = this.state;
@@ -54,7 +88,12 @@ class View extends React.Component {
           </Button>
         </Form>
         <br />
-        <LogoComponent textL1={textL1} textL2={textL2} />
+        <Button onClick={this.getScreenshotHandler}>Get Screenshot!</Button>
+        <br />
+        <div ref={this.captureRef}>
+          <LogoComponent textL1={textL1} textL2={textL2} />
+        </div>
+        <div ref={this.displayRef} />
       </>
     );
   }
