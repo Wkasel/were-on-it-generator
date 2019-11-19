f1a547eb929fbe7b3de702d259ba7f20f4ec01a7
William Kasel <wkasel@gmail.com>
2019-11-19 12:59:11 -0800

    Added Stylelint

---
+++ src/components/Slogan/Slogan.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Slogan = () => {
+  return <h1>Slogan</h1>;
+};
+
+export { Slogan };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/theme.scss';
+import './assets/styles/theme.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
